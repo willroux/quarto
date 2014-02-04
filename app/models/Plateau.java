6c8a45a2226a4f4d34bc8a8cff69938172c2a07c
Quentin Barbosa <quentin.barbosa@etu.univ-orleans.fr>
2014-02-04 13:18:44 +0100

    Ajout des accesseurs de Plateau pour passer les tests.

--- app/models/Plateau.java
+++ app/models/Plateau.java
@@ -14,6 +14,34 @@ public class Plateau {
 		init();
 	}
 
-	
+	/**
+	 *	Initialise le Plateau (cases vides)
+	 */
+	public void init(){
+		for(int i=0; i<taille; ++i) {
+			for(int j=0; j<taille; ++j) {
+				grille[i][j] = new Case();
+			}
+		}
+	}
+
+
+	/**
+	 *	Permet une écriture du type : p.getGrille()[2][3];
+	 */
+	public Case[][] getGrille() {
+		return grille;
+	}
+
+	/**
+	 * 	Permet une écriture du type : p.getGrille(2,3);
+	 */
+	public Case getGrille(int x, int y){
+		return grille[x][y];
+	}
+
+	public int getTaille() {
+		return taille;
+	}
 
 }
