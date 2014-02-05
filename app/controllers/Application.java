c51d7fd2b577db0c1390e0c87b606441922bf5a0
william roux <warriorking44@hotmail.fr>
2014-02-05 12:45:02 +0100

    Affichage du plateau pour tester interaction controllers-models en scala

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,13 +2,16 @@ package controllers;
 
 
 import play.mvc.*;
-
+import play.data.*;
 import views.html.*;
+import models.*;
 
 public class Application extends Controller {
 
+	final static Plateau plateau = new Plateau(); 
+
     public static Result index() {
-        return ok(index.render());
+        return ok(index.render(plateau));
     }
 
 }
